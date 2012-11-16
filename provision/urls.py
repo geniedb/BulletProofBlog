8a999ed7b002fcd307093cc07cc4ec0871a682db
David Newgas <david@geniedb.com>
2012-11-16 16:18:06 +0000

    Add cleanup action
    A cron job can hit http://demo.geniedb.com/cleanup and all necessary demos
    will be shutdown.

--- provision/urls.py
+++ provision/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('provision.views',
      url(r'^$', 'request'),
+     url(r'^cleanup$', 'shutdown_old'),
      url(r'^launch/(?P<demo_id>\d+:[-\w]+)/$', 'launch')
 )
--- provision/views.py
+++ provision/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, get_object_or_404
 from provision.models import Demo
 from django.core.signing import Signer
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 from django.core.mail import mail_admins
+from django.utils import timezone
+import datetime
 
 def request(req):
     try:
@@ -22,3 +24,9 @@ def launch(req, demo_id):
         return render(req,'provision/launched.html', {'demo': d})
     else:
         return render(req,'provision/launch.html', {'demo': d})
+
+def shutdown_old(req):
+    # doesn't require perms
+    for d in Demo.objects.filter(shutdown__exact=None, launched__lte=timezone.now() - datetime.timedelta(hours=1)):
+        d.do_shutdown()
+    return HttpResponse()
