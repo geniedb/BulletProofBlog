c467d13c4be4aace5cea560a6b14ecabd4144a66
David Newgas <david@geniedb.com>
2012-11-16 16:06:29 +0000

    Model: Send email on approval

--- provision/models.py
+++ provision/models.py
@@ -1,6 +1,16 @@
 import datetime
 from django.db import models
 from django.utils import timezone
+from django.core.mail import send_mail
+from django.contrib.sites.models import Site
+from django.core.signing import Signer
+
+DEMO_MESSAGE = """Your CloudFabric Demo has been approved
+
+Please visit http://{domain}{path} to launch your demo.
+--
+GenieDB
+"""
 
 class Demo(models.Model):
     name = models.CharField(max_length=200)
@@ -15,6 +25,11 @@ class Demo(models.Model):
 
     def __unicode__(self):
         return "{name} ({organization}) <{email}>".format(name=self.name, organization=self.organization, email=self.email)
+
+    @models.permalink
+    def get_absolute_url(self):
+        return ('provision.views.launch', [Signer().sign(self.pk)])
+
     def due_to_shudown(self):
         return self.launched <= timezone.now() - datetime.timedelta(hours=1)
 
@@ -23,6 +38,11 @@ class Demo(models.Model):
 
     def do_approve(self):
         self.approved = timezone.now()
+        send_mail("CloudFabric Demo Ready",
+                  DEMO_MESSAGE.format(domain=Site.objects.get_current().domain, path=self.get_absolute_url()),
+                  'tech@geniedb.com',
+                  [self.email])
+        self.save()
 
     def do_launch(self):
         self.launched = timezone.now()
